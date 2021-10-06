0b0102b1e3a60452be0cde3bb8f6590ac8817996
Renaud Bourdeau <renaudbourdeau@gmail.com>
2021-10-06 11:01:51 +0200

    feat(livereload): insert livereload in the project

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,32 @@ import resolve from 'rollup-plugin-node-resolve';
 import scss from 'rollup-plugin-scss';
 import { terser } from "rollup-plugin-terser";
 import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+
+
+let plugins = [
+    resolve(),
+    commonjs({
+        include: 'node_modules/**',
+    }),
+    babel({
+        exclude: 'node_modules/**',
+    }),
+    terser(),
+    scss({
+        output:'public/css/style.css',
+        failOnError: true,
+        watch: 'front/scss/',
+        outputStyle: "compressed",
+    })
+];
+if(process.argv.includes('dev')){
+    plugins.splice(0, 0, livereload({
+        verbose: true,
+        port: 9000,
+        delay: 500,
+    }));
+}
 
 export default {
     input: 'front/js/index.js',
@@ -11,20 +37,5 @@ export default {
         file: 'public/js/app.js',
         name: 'app'
     },
-    plugins: [
-        resolve(),
-        commonjs({
-            include: 'node_modules/**',
-        }),
-        babel({
-            exclude: 'node_modules/**',
-        }),
-        terser(),
-        scss({
-            output:'public/css/style.css',
-            failOnError: true,
-            watch: 'front/scss/',
-            outputStyle: "compressed",
-        })
-    ]
-}
+    plugins:plugins
+};
